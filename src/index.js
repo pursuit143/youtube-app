f6f9c8e17c160261da116e29daede25c801ab25d
Soyel Rana <soyelrana143@gmail.com>
2025-01-28 18:05:35 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 // Load environment variables
 
 dotenv.config({
   path: "./env"
 });
-
 const PORT = process.env.PORT || 5000;
-
-// Define Express app
 // Connect to MongoDB
 
-connectDB();
+connectDB()
+ .then(() => {
+  app.listen(PORT, ()=>{
+    console.log(`Server running on port http://localhost:${PORT}`);
+    console.log("MongoDB Connected...");
+  })
+  })
+  .catch((err) => {console.log('Error connecting to MongoDB', err);});
